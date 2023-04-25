4dc0258efce7fa451eaba8d66c2aeef685ebbd4b
= <=>
2023-04-25 18:02:53 +0200

    add container

--- searos/myApp/views.py
+++ searos/myApp/views.py
@@ -4,6 +4,9 @@ import os
 
 from django.conf import settings
 from os import system
+from time import sleep
+
+
 
 def homepage(request):
     context = {}
@@ -18,14 +21,21 @@ def upload_file(request):
         return render(request, 'upload_file.html', {'message': 'File uploaded successfully.'})
     return render(request, 'upload_file.html')
 
+
+
 def run_stat(request, filename):
     filepath = os.path.join(settings.MEDIA_ROOT, filename)
-    system("stat "+filepath + ">output.txt")
+    init = "docker start seahorn_web"
+    command = f"docker exec seahorn_web sea pf -m64 /host/{filename} | tail -n 1 > output_sea.txt"
+    #create = f"docker run  --name=seahorn_web -v {settings.BASE_DIR}/MEDIA:/host seahorn/seahorn-llvm5"
+    #system(create)
+    system(init)
+    sleep(3)
+    system(command)
 
-    with open("output.txt") as f:
+    with open("output_sea.txt") as f:
         data = f.read()
     
     system("rm "+filepath)
-
-
+    system("docker stop seahorn_web")
     return HttpResponse(data)
