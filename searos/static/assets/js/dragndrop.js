86761661e1d940eb337f0bdb1e814da58c671ec7
Typhaine HAUSER <typh@MacBook-Pro-de-Typhaine.local>
2023-04-26 16:06:49 +0200

    fix drag n drop

--- searos/static/assets/js/dragndrop.js
+++ searos/static/assets/js/dragndrop.js
@@ -43,19 +43,19 @@ dropArea.addEventListener("drop", (event)=>{
   });
 
 function showFile(){
-    let fileType = file.type; //getting selected file type
-    let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
-    if(validExtensions.includes(fileType)){ //if user selected file is an image file
-      let fileReader = new FileReader(); //creating new FileReader object
-      fileReader.onload = ()=>{
-        let fileURL = fileReader.result; //passing user file source in fileURL variable
-        let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
-        dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
-      }
-      fileReader.readAsDataURL(file);
-    }else{
-      alert("This is not an Image File!");
+    /*let fileType = file.type; //getting selected file type
+    let validExtensions = ["\.c", "\.h"]; //adding some valid c extensions in array*/
+    /*if(validExtensions.includes(fileType)){ //if user selected file is an image file*/
+    let fileReader = new FileReader(); //creating new FileReader object
+    fileReader.onload = ()=>{
+      let fileURL = fileReader.result; //passing user file source in fileURL variable
+      let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
+      dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
+    }
+    fileReader.readAsDataURL(file);
+    /*}else{
+      alert("This is not a C File!");
       dropArea.classList.remove("active");
       dragText.textContent = "Drag & Drop to Upload File";
-    }
+    }*/
   }
